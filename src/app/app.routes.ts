c1e0d58b7be7967ce207ec169977956053266c4d
Gabriele Napoli <gabriele.nap@gmail.com>
2024-01-26 12:52:31 +0100

    add layout component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './@pages/home/home.component';
 import { LoginComponent } from './@pages/login/login.component';
+import { AuthGuard } from './@core/guards/auth.guard';
+import { AutoLoginGuard } from './@core/guards/auto-login.guard';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'login',
     component: LoginComponent,
+    canActivate: [AutoLoginGuard],
   },
 ];
